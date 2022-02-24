2b89430015acb5ca056d0a467877c26317fa4886
glenchiridza <glenchiridz@gmail.com>
2022-02-24 16:20:17 +0200

    controls

--- src/SwipeButtons.js
+++ src/SwipeButtons.js
@@ -1,10 +1,30 @@
+import { IconButton } from '@material-ui/core'
+import { Replay, Close, Favorite, FlashOn, StarRate } from '@material-ui/icons'
 import React from 'react'
 import './SwipeButtons.css'
 
 function SwipeButtons() {
+    function swiped (){
+
+    }
+
   return (
     <div className='swipeButtons'>
-
+        <IconButton>
+            <Replay onClick={swiped} fontSize="large" className="btn-replay"/>
+        </IconButton>
+        <IconButton>
+            <Close onClick={swiped} fontSize="large" className="btn-close"/>
+        </IconButton>
+        <IconButton>
+            <StarRate onClick={swiped} fontSize="large" className="btn-rate"/>
+        </IconButton>
+        <IconButton>
+            <Favorite onClick={swiped} fontSize="large" className="btn-fav"/>
+        </IconButton>
+        <IconButton>
+            <FlashOn onClick={swiped} fontSize="large" className="btn-flashon"/>
+        </IconButton>
     </div>
   )
 }
