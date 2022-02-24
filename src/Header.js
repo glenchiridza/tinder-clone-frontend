360cd8a9c4a5243d8c6b500bcc60747ca9cf2d2c
glenchiridza <glenchiridz@gmail.com>
2022-02-24 14:59:01 +0200

    tinder cards start

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,23 @@
+import { IconButton } from '@material-ui/core';
+import { Forum, Person } from '@material-ui/icons';
 import React from 'react';
 import './Header.css';
 
 function Header() {
   return (
     <div className='header'>
-My heaer
+        
+        <IconButton>
+            <Person font-size="large" className="header_icon" />
+        </IconButton>
+
+        <img className='header_logo'
+            src='images/tinder_logo.png' alt='header logo'/>
+        
+        <IconButton>
+            <Forum font-size="large" className='header_icon' />
+        </IconButton>
+
     </div>
   )
 }
---
+++ src/TinderCards.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TinderCards.css'
+
+function TinderCards() {
+  return (
+    <div className='tinder_cards'>
+        TinderCards
+        </div>
+  )
+}
+
+export default TinderCards
