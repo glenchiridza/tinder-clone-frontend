5d595aa35309fdf1465fd0a653f563e5b28cda69
glenchiridza <glenchiridz@gmail.com>
2022-02-24 14:28:08 +0200

    tinder start

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+
+    <div className="App">
+      
+      <Header />
+
+      {/* tindercards */}
+
+      {/* swipebuttons */}
+
+
+    </div>
+  );
+}
+
+export default App;
